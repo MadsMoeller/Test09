26890f03b704193966ae59de1447d2620d00b6d6
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2022-01-24 13:06:57 +0100

    Initial commit

---
+++ src/main/java/tek/examprep/test09/Test09Application.java
@@ -0,0 +1,13 @@
+package tek.examprep.test09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test09Application.class, args);
+    }
+
+}
